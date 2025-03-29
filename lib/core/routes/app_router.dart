0239ba8ef30b353eb206781d4e18a061f43b495f
makruban <co1@ukr.net>
2025-03-29 17:56:41 +0200

    moved to navigation 2

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,21 +3,92 @@ import '../../presentation/screens/main_screen.dart';
 import '../../presentation/screens/all_items_screen.dart';
 import '../../presentation/screens/specific_list_screen.dart';
 
-class AppRouter {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_) => const MainScreen());
-      case '/all-items':
-        return MaterialPageRoute(builder: (_) => const AllItemsScreen());
-      case '/specific-list':
-        return MaterialPageRoute(builder: (_) => const SpecificListScreen());
-      default:
-        return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(child: Text('No route defined for ${settings.name}')),
+// Route configuration
+class RouteConfiguration {
+  final String location;
+  final Map<String, dynamic>? arguments;
+
+  RouteConfiguration({
+    required this.location,
+    this.arguments,
+  });
+}
+
+// Router Delegate
+class AppRouterDelegate extends RouterDelegate<RouteConfiguration>
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin<RouteConfiguration> {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+  String _currentPath = '/';
+  Map<String, dynamic>? _arguments;
+
+  AppRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  RouteConfiguration? get currentConfiguration => RouteConfiguration(
+        location: _currentPath,
+        arguments: _arguments,
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        if (_currentPath == '/')
+          const MaterialPage(
+            key: ValueKey('home'),
+            child: MainScreen(),
+          )
+        else if (_currentPath == '/all-items')
+          const MaterialPage(
+            key: ValueKey('allItems'),
+            child: AllItemsScreen(),
+          )
+        else if (_currentPath == '/specific-list')
+          const MaterialPage(
+            key: ValueKey('specificList'),
+            child: SpecificListScreen(),
           ),
-        );
-    }
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) return false;
+        _currentPath = '/';
+        notifyListeners();
+        return true;
+      },
+    );
+  }
+
+  @override
+  Future<void> setNewRoutePath(RouteConfiguration configuration) async {
+    _currentPath = configuration.location;
+    _arguments = configuration.arguments;
+    notifyListeners();
+  }
+
+  void navigateTo(String path, {Map<String, dynamic>? arguments}) {
+    _currentPath = path;
+    _arguments = arguments;
+    notifyListeners();
+  }
+}
+
+// Route Information Parser
+class AppRouteInformationParser
+    extends RouteInformationParser<RouteConfiguration> {
+  @override
+  Future<RouteConfiguration> parseRouteInformation(
+      RouteInformation routeInformation) async {
+    final uri = Uri.parse(routeInformation.location ?? '/');
+    return RouteConfiguration(
+      location: uri.path,
+      arguments: null,
+    );
+  }
+
+  @override
+  RouteInformation? restoreRouteInformation(RouteConfiguration configuration) {
+    return RouteInformation(location: configuration.location);
   }
 }
